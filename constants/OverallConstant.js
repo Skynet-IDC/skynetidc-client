4cc5787d00c03dadca3572786e24962371a6dffd
Hoa Doan <hoadq@skynetidc.vn>
2025-10-15 13:34:40 +0700

    Update code submit writing

---
+++ constants/OverallConstant.js
@@ -0,0 +1,5 @@
+module.exports = {
+   ELEMENTARY: 'Còn Hạn Chế',
+   INTERMEDIATE: 'Đang Tiến Bộ',
+   PROFICIENT: 'Thành Thạo',
+}
--- services/TestResultService.js
+++ services/TestResultService.js
@@ -1,6 +1,7 @@
 const userLearningInfoRepository = require("../repositories/UserLearningInfoRepository");
 const utils = require("../utils/CommonUtils");
 const reviewConst = require("../constants/ReviewConstant");
+const overallConst = require("../constants/OverallConstant");
 const knowledgeConst = require("../constants/KnowledgeConstant");
 module.exports = {
     getReport: async function (levelId, profileId) {
@@ -19,6 +20,12 @@ module.exports = {
         let listeningReview = '';
         let grammaReview = '';
         let vocabularyReview = '';
+        let writingOverall = '';
+        let readingOverall = '';
+        let speakingOverall = '';
+        let listeningOverall = '';
+        let grammaOverall = '';
+        let vocabularyOverall = '';
         let model = await userLearningInfoRepository.findByLevelIdAndProfileId(levelId, profileId);
 
         if (!utils.isEmpty(model) && !utils.isEmpty(model.testResults) ) {
@@ -46,52 +53,70 @@ module.exports = {
                         writingScore += 1;
                     }
                 });
-                if (vocabularyScore > 0 && vocabularyScore <= 2) {
+                if (vocabularyScore >= 0 && vocabularyScore <= 2) {
                     vocabularyReview = reviewConst.VOCABULARY_ELEMENTARY;
+                    vocabularyOverall = overallConst.ELEMENTARY;
                 } else if (vocabularyScore > 2 && vocabularyScore <= 4) {
                     vocabularyReview = reviewConst.VOCABULARY_INTERMEDIATE;
+                    vocabularyOverall = overallConst.INTERMEDIATE;
                 } else if (vocabularyScore === 5) {
                     vocabularyReview = reviewConst.VOCABULARY_PROFICIENT;
+                    vocabularyOverall = overallConst.PROFICIENT;
                 }
 
-                if (grammaScore > 0 && grammaScore <= 2) {
+                if (grammaScore >= 0 && grammaScore <= 2) {
                     grammaReview = reviewConst.GRAMMA_ELEMENTARY;
+                    grammaOverall = overallConst.ELEMENTARY;
                 } else if (grammaScore > 2 && grammaScore <= 4) {
                     grammaReview = reviewConst.GRAMMA_INTERMEDIATE;
+                    grammaOverall = overallConst.INTERMEDIATE;
                 } else if (grammaScore === 5) {
                     grammaReview = reviewConst.GRAMMA_PROFICIENT;
+                    grammaOverall = overallConst.PROFICIENT;
                 }
 
-                if (listeningScore > 0 && listeningScore <= 1) {
+                if (listeningScore >= 0 && listeningScore <= 1) {
                     listeningReview = reviewConst.LISTENING_ELEMENTARY;
+                    listeningOverall = overallConst.ELEMENTARY;
                 } else if (listeningScore === 2) {
                     listeningReview = reviewConst.LISTENING_INTERMEDIATE;
+                    listeningOverall = overallConst.INTERMEDIATE;
                 } else if (listeningScore === 3) {
                     listeningReview = reviewConst.LISTENING_PROFICIENT;
+                    listeningOverall = overallConst.PROFICIENT;
                 }
 
-                if (speakingScore > 0 && speakingScore <= 2) {
+                if (speakingScore >= 0 && speakingScore <= 2) {
                     speakingReview = reviewConst.SPEAKING_ELEMENTARY;
+                    speakingOverall = overallConst.ELEMENTARY;
                 } else if (speakingScore > 2 && speakingScore <= 4) {
                     speakingReview = reviewConst.SPEAKING_INTERMEDIATE;
+                    speakingOverall = overallConst.INTERMEDIATE;
                 } else if (speakingScore === 5) {
                     speakingReview = reviewConst.SPEAKING_PROFICIENT;
+                    speakingOverall = overallConst.PROFICIENT;
                 }
 
-                if (readingScore > 0 && readingScore <= 1) {
+                if (readingScore >= 0 && readingScore <= 1) {
                     readingReview = reviewConst.READING_ELEMENTARY;
+                    readingOverall = overallConst.ELEMENTARY;
                 } else if (readingScore === 2) {
                     readingReview = reviewConst.READING_INTERMEDIATE;
+                    readingOverall = overallConst.INTERMEDIATE;
                 } else if (readingScore === 3) {
                     readingReview = reviewConst.READING_PROFICIENT;
+                    readingOverall = overallConst.PROFICIENT;
                 }
 
-                if (writingScore > 0 && writingScore <= 1) {
+                if (writingScore >= 0 && writingScore <= 1) {
                     writingReview = reviewConst.WRITING_ELEMENTARY;
+                    writingOverall = overallConst.ELEMENTARY;
                 } else if (writingScore === 2) {
                     writingReview = reviewConst.WRITING_INTERMEDIATE;
+                    writingOverall = overallConst.INTERMEDIATE;
                 } else if (writingScore === 3) {
                     writingReview = reviewConst.WRITING_PROFICIENT;
+                    writingOverall = overallConst.PROFICIENT;
                 }
 
                 totalScore = vocabularyScore + grammaScore + listeningScore + speakingScore + readingScore + writingScore;
@@ -125,14 +150,17 @@ module.exports = {
                 reading: readingScore*(10/3), // Tối đa 3 điểm
                 writing: writingScore*(10/3) // Tối đa 3 điểm
             },
-            detailReview: {
-                vocabulary: vocabularyReview,
-                gramma: grammaReview,
-                listening: listeningReview,
-                speaking: speakingReview,
-                reading: readingReview,
-                writing: writingReview
-            }
+            overall: {
+                vocabulary: vocabularyOverall,
+                gramma: grammaOverall,
+                listening: listeningOverall,
+                speaking: speakingOverall,
+                reading: readingOverall,
+                writing: writingOverall
+            },
+            reviews: [
+                vocabularyReview, grammaReview, listeningReview, speakingReview, readingReview, writingReview
+            ]
         };
     }
 }
